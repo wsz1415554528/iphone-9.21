c88d0683c9dd7c03ed5c6bf316cb933f1eda369f
wsz1415554528 <s20161104602@mac22.local>
2018-09-21 17:08:09 +0800

    Initial Commit

---
+++ iphone 9.21/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iphone 9.21
+//
+//  Created by s20161104602 on 2018/9/21.
+//  Copyright © 2018年 s20161104602. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
