2490aad08e84e2a6445dd68aa6074c3a7568fdf0
wsz1415554528 <s20161104602@mac22.local>
2018-10-10 11:41:22 +0800

    xiugai

--- iphone 9.21/ViewController.swift
+++ iphone 9.21/ViewController.swift
@@ -10,42 +10,143 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var text: UITextField!
+    @IBOutlet weak var lableResult: UILabel!
     
-    @IBOutlet weak var number1: UIButton!
-    
-    @IBOutlet weak var number2: UIButton!
-    
-    @IBOutlet weak var number3: UIButton!
-    
-    @IBOutlet weak var number4: UIButton!
-    
-    @IBOutlet weak var number5: UIButton!
-    
-    @IBOutlet weak var number6: UIButton!
-    
-    @IBOutlet weak var number7: UIButton!
-    
-    @IBOutlet weak var number8: UIButton!
-    
-    @IBOutlet weak var number9: UIButton!
-    
-    @IBOutlet weak var number0: UIButton!
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    var firstOperand: Double = 0.0
+
+    var secondOperand: Double = 0.0
+  
+    var decimalPointFlag: Bool = false
+  
+    var isSecond: Bool = false
+  
+    var operatorFlag: String = ""
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+    @IBAction func buttonTap(sender: UIButton) {
+       
+        if lableResult.text == "0" || (isSecond && secondOperand == 0.0) {
+            
+            lableResult.text = ""
+        }
+       
+        lableResult.text = lableResult.text! + sender.titleLabel!.text!
+        
+        if isSecond {
+            
+            secondOperand = NSString(string: lableResult.text!).doubleValue
+        }else {
+           
+            firstOperand = NSString(string: lableResult.text!).doubleValue
+        }
+        print(firstOperand)
+    }
+    
+    
+    @IBAction func decimalPointTap() {
+        
+        if !decimalPointFlag {
+            lableResult.text = lableResult.text! + "."
+            if isSecond {
+                secondOperand = (lableResult.text! as NSString).doubleValue
+            }else {
+                firstOperand = (lableResult.text! as NSString).doubleValue
+            }
+            
+            decimalPointFlag = !decimalPointFlag
+        }
+    }
+    
+    
+    @IBAction func operatorTap(sender: UIButton) {
+        if firstOperand != 0 {
+            isSecond = true
+            decimalPointFlag = false
+            switch sender.titleLabel!.text! {
+            case "+":
+                operatorFlag = "+"
+            case "-":
+                operatorFlag = "-"
+            case "*":
+                operatorFlag = "*"
+            case "÷":
+                operatorFlag = "/"
+            default:
+                operatorFlag = " "
+            }
+        }
+    }
+    
+    
+    @IBAction func resultTap(sender: UIButton) {
+        //确保第二操作数有值
+        if isSecond {
+            //除数不能为0
+            if operatorFlag == "/" && secondOperand == 0 {
+                print("Error: 除数不能为0")
+                return
+            }
+            
+            var result: Double = 0.0
+            switch operatorFlag {
+            case "+":
+                result = firstOperand + secondOperand
+            case "-":
+                result = firstOperand - secondOperand
+            case  "*":
+                result = firstOperand * secondOperand
+            case  "/":
+                result = firstOperand / secondOperand
+            default :
+                result = 0.0
+            }
+            
+            lableResult.text = result.description
+            print("第一操作: \(firstOperand)")
+            print("操作符: \(operatorFlag)")
+            print("第二操作: \(secondOperand)")
+            print("结果: \(result)")
+            
+        }
+        
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        
+       
+        lableResult.text = "0"
+        
+        
+        firstOperand = 0.0
+        
+       
+        secondOperand = 0.0
+        
+        
+        decimalPointFlag = false
+        
+        
+        isSecond = false
+        
+        
+        operatorFlag = ""
+        
+    }
+}
+func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
-}
+
 
